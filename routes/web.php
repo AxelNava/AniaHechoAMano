bc4fb1889b9503f3e7ef29d05270a353d2457433
AxelNava <napa20020314@gmail.com>
2025-08-06 11:20:37 -0600

    feat: Actualización de dependencias y nuevas rutas (main)
    Se actualizaron las dependencias del proyecto
    mediante composer, añadiendo nuevas librerías y
    actualizando versiones existentes.  Además, se
    crearon nuevas rutas para la vista principal,
    búsqueda de productos y una vista inicial. Se
    añadieron también nuevos archivos SVG para el
    logo del proyecto.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/up', function () {
+    return view('first_view');
+});
+
+Route::get("/search-products", function () {
+    return view('/SearchProduct');
+})->name('search-products');
