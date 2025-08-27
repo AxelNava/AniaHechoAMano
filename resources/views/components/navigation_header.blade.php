5458a6c4fe3bdd907acff614ea26e1aae608c5e7
AxelNava <napa20020314@gmail.com>
2025-08-27 08:55:02 -0600

    feat: Seeding y mejoras en la navegación (main)
    Se implementó la siembra de datos para categorías y
    productos. Se realizaron mejoras en la navegación,
    incluyendo estilos y ajustes en la presentación de
    los elementos. Se añadió la librería `barryvdh/laravel-ide-helper`
    para mejorar la autocompleción del IDE. Se agregaron nuevas
    fuentes  y se actualizaron las dependencias del proyecto.

--- resources/views/components/navigation_header.blade.php
+++ resources/views/components/navigation_header.blade.php
@@ -1,5 +1,5 @@
-<nav class="max-lg:relative max-lg:h-22 ">
-    <ul class="max-lg:absolute max-lg:grid-cols-[auto_auto_auto] max-lg:grid-rows-3 grid max-lg:inset-x-10 max-sm:inset-x-0 max-sm:px-0 max-sm:gap-x-2 max-sm:*:*:text-[0.5rem] max-sm:*:*:px-2 lg:grid-flow-col gap-2 justify-items-center justify-center py-4 navigation-buttons transition-transform">
+<nav class="max-lg:relative">
+    <ul class="max-lg:bg-white max-sm:px-2 max-lg:absolute max-lg:grid-cols-[auto_auto_auto] grid max-lg:w-[100dvw] max-sm:gap-x-2 max-sm:*:*:text-[0.75rem] max-sm:*:*:px-2 lg:grid-flow-col max-lg:gap-y-8 lg:gap-4 justify-items-center items-center justify-center py-4 navigation-buttons transition-transform">
         <li>
             <x-primary-button href="" selected>Piñatas</x-primary-button>
         </li>
@@ -19,24 +19,39 @@
             <x-primary-button href="">Desayunos sorpresa</x-primary-button>
         </li>
         <li>
-            <x-primary-button href="">Fotos polaroid</x-primary-button>
+            <x-primary-button href="" class="last">Fotos polaroid</x-primary-button>
         </li>
         <li>
-            <x-primary-button href="">Cajas sorpresa</x-primary-button>
+            <x-primary-button href="" class="last">Cajas sorpresa</x-primary-button>
         </li>
         <li>
             <input type="checkbox" id="others" class="hidden checkbox-other">
-            <label for="others" class="button-others lg:hidden text-text-page text-center border-2 border-secondary font-sans-serif text-sm px-4 py-2 inline-block rounded-4xl transition-[color,background-color,translate] duration-200 hover:-translate-y-1">Otros</label>
+            <label for="others"
+                   class="last button-others lg:hidden text-text-page text-center border-2 border-secondary font-sans-serif text-sm px-4 py-2 inline-block rounded-4xl transition-[color,background-color,translate] duration-200 hover:-translate-y-1">Otros</label>
         </li>
         {{--        <li>buscar</li>--}}
     </ul>
 </nav>
 
 <script>
-   let inputCheckOther = document.querySelector(".checkbox-other")
-   let navigationUl=document.querySelector(".navigation-buttons")
-   if(inputCheckOther !== null){
-       if(inputCheckOther.checked){
-       }
-   }
+    let inputCheckOther = document.querySelector(".checkbox-other")
+    let navigationUl = document.querySelector(".navigation-buttons")
+    let lastRowGrid = document.querySelectorAll(".navigation-buttons .last")
+    if(lastRowGrid !== null){
+        let parentsElementList = lastRowGrid.entries().map((element) => element.parent)
+
+    }
+    if (inputCheckOther !== null) {
+        if (inputCheckOther.checked) {
+        } else {
+            lastRowGrid.forEach((element)=>
+            {
+                // element.style.cssText='margin-bottom:2rem;'
+                console.log(element)
+            })
+            // navigationUl.style.cssText = 'transform:translateY(-18dvh);'
+            console.log(lastRowGrid)
+
+        }
+    }
 </script>
