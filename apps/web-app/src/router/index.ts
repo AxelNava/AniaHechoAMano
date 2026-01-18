d1a74ebf5477a20b85b19bce4af5e47ea1efbe11
AxelNava <napa20020314@gmail.com>
2026-01-18 00:00:56 -0600

    refactor(router): Rename `welcome.vue` to `WelcomeView.vue` for consistency in file naming

--- apps/web-app/src/router/index.ts
+++ apps/web-app/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WelcomePage from '../pages/welcome.vue'
+import WelcomePage from '../views/WelcomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
